2ae4bf7fb0c87530eebeb6b73306c46ecf3b692b
Sagar Tandan <076bct072@ioepc.edu.np>
2024-05-16 17:39:31 +0545

    removed API key

--- src/Youtube/Home.jsx
+++ src/Youtube/Home.jsx
@@ -11,10 +11,9 @@ export default function Home() {
 
   const [loading, setLoading] = useState("False"); // Add loading state
 
-  // let API_KEY = "AIzaSyCqSY3vDMPkjVDbhbHheROw2vrzu7e6Q-w"; //w
-  let API_KEY = import.meta.env.VITE_APP_API_KEY1; //w
+  let API_KEY = import.meta.env.VITE_APP_API_KEY1; 
   let API_KEY1 = import.meta.env.VITE_APP_API_KEY2;
-  let API_KEY2 = import.meta.env.VITE_APP_API_KEY3; //o
+  let API_KEY2 = import.meta.env.VITE_APP_API_KEY3; 
   let API_KEY3 = import.meta.env.VITE_APP_API_KEY4;
 
   let regExp =
